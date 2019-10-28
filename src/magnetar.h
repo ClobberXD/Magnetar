a0ead8b467722d70761551c391c6c0ef4a5e719a
ANAND <ClobberXD@gmail.com>
2019-10-28 18:46:36 +0530

    Add PCH support using cotire CMake module

--- src/magnetar.h
+++ src/magnetar.h
@@ -1,7 +1,6 @@
-// Master include file, to include all Magnetar files
+// Master include file, to be used by clients
 
 #include "magnetar/application.h"
 #include "magnetar/entry_point.h"
 
-// Miscellaneous headers
 #include "magnetar/log.h"
---
+++ src/magnetar_pch.h
@@ -0,0 +1,16 @@
+// Single pre-compiled header for use by Magnetar
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <functional>
+#include <utility>
+#include <algorithm>
+
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
